0768ef706bf085b8322f792291012381478f2315
oscaralvarezsanz <oscaralvarezsanz14@gmail.com>
2026-06-09 19:41:27 +0200

    refactor: remove unused GroupGetByIdRepository from PredictionGetService

--- src/main/java/com/topleague/predict/application/service/prediction/PredictionGetService.java
+++ src/main/java/com/topleague/predict/application/service/prediction/PredictionGetService.java
@@ -1,15 +1,10 @@
 package com.topleague.predict.application.service.prediction;
 
 import com.topleague.predict.application.port.in.prediction.GetMyGroupPredictionsUseCase;
-import com.topleague.predict.application.port.out.group.GroupGetByIdRepository;
 import com.topleague.predict.application.port.out.groupmember.GroupMemberGetByGroupIdRepository;
 import com.topleague.predict.application.port.out.prediction.PredictionGetByGroupAndUserRepository;
-import com.topleague.predict.domain.exception.GroupErrorCode;
-import com.topleague.predict.domain.exception.GroupException;
 import com.topleague.predict.domain.exception.GroupMemberErrorCode;
 import com.topleague.predict.domain.exception.GroupMemberException;
-import com.topleague.predict.domain.model.Group;
-import com.topleague.predict.domain.model.GroupMember;
 import com.topleague.predict.domain.model.Prediction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,15 +14,12 @@ import java.util.List;
 @Service
 public class PredictionGetService implements GetMyGroupPredictionsUseCase {
 
-    private final GroupGetByIdRepository groupGetByIdRepository;
     private final GroupMemberGetByGroupIdRepository groupMemberGetByGroupIdRepository;
     private final PredictionGetByGroupAndUserRepository predictionGetByGroupAndUserRepository;
 
     public PredictionGetService(
-            GroupGetByIdRepository groupGetByIdRepository,
             GroupMemberGetByGroupIdRepository groupMemberGetByGroupIdRepository,
             PredictionGetByGroupAndUserRepository predictionGetByGroupAndUserRepository) {
-        this.groupGetByIdRepository = groupGetByIdRepository;
         this.groupMemberGetByGroupIdRepository = groupMemberGetByGroupIdRepository;
         this.predictionGetByGroupAndUserRepository = predictionGetByGroupAndUserRepository;
     }
